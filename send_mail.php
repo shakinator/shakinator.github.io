11ee9e959d19e83246019dc23fe4296cc5908ff0
Saksham_garg <65782416+shakinator@users.noreply.github.com>
2023-04-15 13:47:30 +0530

    Update send_mail.php
    commit 8

--- send_mail.php
+++ send_mail.php
@@ -29,42 +29,6 @@ $msg =
 The following function checks for email injection.
 Specifically, it checks for carriage returns - typically used by spammers to inject a CC list.
 */
-function isInjected($str) {
-	$injections = array('(\n+)',
-	'(\r+)',
-	'(\t+)',
-	'(%0A+)',
-	'(%0D+)',
-	'(%08+)',
-	'(%09+)'
-	);
-	$inject = join('|', $injections);
-	$inject = "/$inject/i";
-	if(preg_match($inject,$str)) {
-		return true;
-	}
-	else {
-		return false;
-	}
-}
-
-// If the user tries to access this script directly, redirect them to the feedback form,
-if (!isset($_REQUEST['email_address'])) {
-header( "Location: $feedback_page" );
-}
-
-// If the form fields are empty, redirect to the error page.
-elseif (empty($first_name) || empty($email_address)) {
-header( "Location: $error_page" );
-}
-
-/* 
-If email injection is detected, redirect to the error page.
-If you add a form field, you should add it here.
-*/
-elseif ( isInjected($email_address) || isInjected($first_name)  || isInjected($comments) ) {
-header( "Location: $error_page" );
-}
 
 // If we passed all previous tests, send the email then redirect to the thank you page.
 else {
